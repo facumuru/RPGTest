d1af876423adb177134cacaeebf65143138e5208
Facundo Murua <facumuru123@gmail.com>
2023-10-23 20:48:03 -0300

    Fix errors

---
+++ CaveSimpleTest/cave/output/cave-extra.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="cave-extra" tilewidth="16" tileheight="16" tilecount="2000" columns="50">
+ <image source="cave-extra.png" width="800" height="640"/>
+ <tile id="150">
+  <properties>
+   <property name="description" value="a cave entrance."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="description" value="a cave entrance."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="description" value="a cave entrance."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="200">
+  <properties>
+   <property name="description" value="a cave entrance."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="201">
+  <properties>
+   <property name="description" value="a cave entrance."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <properties>
+   <property name="description" value="a cave entrance."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ CaveSimpleTest/cave/output/caves-holes-ladders.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="caves-holes-ladders" tilewidth="16" tileheight="16" tilecount="12" columns="3">
+ <image source="caves-holes-ladders.png" width="48" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="description" value="a cave entrance with a ladder."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="description" value="a hole with a ladder."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="description" value="a cave entrance."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="description" value="a ladder."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="description" value="a cave exit (requires a rope)."/>
+   <property name="ge_collide" type="bool" value="false"/>
+   <property name="usewith_origin_item_key" value="rope"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="description" value="a cave exit (requires a rope)."/>
+   <property name="ge_collide" type="bool" value="false"/>
+   <property name="usewith_origin_item_key" value="rope"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="description" value="a ladder."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="description" value=""/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="description" value="a cave exit (requires a rope)."/>
+   <property name="ge_collide" type="bool" value="false"/>
+   <property name="usewith_origin_item_key" value="rope"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="description" value="a ladder."/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="description" value=""/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="description" value=""/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
