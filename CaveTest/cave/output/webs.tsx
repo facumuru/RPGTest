10f84795f308aa3d1a154fa3f8d3b758bff1b604
Facundo Murua <facumuru123@gmail.com>
2023-10-23 19:36:57 -0300

    Add CaveSimple.tmx
    	Add file tmx

---
+++ CaveTest/cave/output/webs.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="webs" tilewidth="16" tileheight="16" tilecount="32" columns="16">
+ <image source="webs.png" width="256" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="description" value="web"/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="description" value="web"/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="description" value="web"/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="description" value="web"/>
+   <property name="ge_collide" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
